2e381a67622eb3f46deb6f9015425c18271f3691
Nasimi Mamed-zade <nasimi@mamed-zade.com>
2020-09-28 13:45:22 +0300

    terraform-2

--- terraform/modules/storage-bucket.tf
+++ terraform/modules/storage-bucket.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
-    bucket     = "<имя бакета>"
-    region     = "us-east-1"
-    key        = "<terraform.tfstate"
-    access_key = "<key>"
-    secret_key = "<secretkey>"
+    bucket     = "testbucket"
+    region     = "ru-central-1"
+    key        = "terraform.tfstate"
+    access_key = "key"
+    secret_key = "secretkey"
 
     skip_region_validation      = true
     skip_credentials_validation = true
